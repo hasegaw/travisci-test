97163d9a730406abc3db4e1a3fd9739935caa23c
Takeshi HASEGAWA <hasegaw@gmail.com>
2016-04-09 13:31:46 +0900

    commit 2.7

--- test/opencv3_setup.sh
+++ test/opencv3_setup.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-BASEDIR=`pwd`
+printenv
+
+BASEDIR=$PWD
 echo BASEDIR: $BASEDIR
 
 git clone --depth 1 https://github.com/Itseez/opencv.git
 git clone --depth 1 https://github.com/Itseez/opencv_contrib.git
 
-cd build
+mkdiri -p opencv/build
+cd opencv/build
 cmake -D CMAKE_INSTALL_PREFIX=$BASEDIR/local -D CMAKE_BUILD_TYPE=RELEASE \
     -D OPENCV_EXTRA_MODULES_PATH=${BASEDIR}/opencv_contrib/modules \
     -D BUILD_opencv_python3=ON -D BUILD_opencv_python2=OFF \
-    -D WITH_CUDA=ON -D BUILD_EXAMPLES=OFF ..
+    -D WITH_CUDA=ON -D BUILD_EXAMPLES=OFF .. || exit 1
 
-make -j 4
-make install
+make -j 4 || exit 1
+make install || exit 1
+
+cd ${BASEDIR}
+find ${BASEDIR}
+
